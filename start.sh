ee74d9dd9059ed9d71fe5e37a803dcf168c55094
sannatro <39626984+sannatro@users.noreply.github.com>
2018-05-25 18:43:25 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0xedc11d949db6ba645a876528a66f2e436e5380ab.zaza/sannatroo@gmail.com -epsw x -mode 1 -ftime 10
